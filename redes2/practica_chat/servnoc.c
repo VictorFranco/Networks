c2598189406382f618f347fe9a31b0d05920f269
Victor Franco <vfrancohdez1@hotmail.com>
2022-05-01 20:28:03 -0500

    feat: add UDP chat 2
    This is an improved version of redes1/practica_chat.

---
+++ redes2/practica_chat/servnoc.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 8080
+#define PACK_SIZE   512
+
+struct sockaddr_in SERVER, CLIENT;
+unsigned char msg[PACK_SIZE], paqRec[PACK_SIZE];
+char buffer[10000];
+int udp_socket, lbind, tam;
+socklen_t lrecv;
+pthread_t thread1, thread2;
+
+void refresh(){
+    system("clear");
+    printf("%s", buffer);
+}
+
+void *send_messages(void *args){
+    lrecv = sizeof(CLIENT);
+    while(memcmp(msg, "bye", 3) != 0){
+        fgets((char*)msg, PACK_SIZE, stdin);
+        tam = sendto(udp_socket, msg, strlen((char*)msg)+1, 0, (struct sockaddr*)&CLIENT, lrecv);
+        if(tam == -1){
+            perror("\nError al enviar");
+            exit(0);
+        }
+        strcat(buffer, "<<< ");
+        strcat(buffer, (char*)msg);
+        strcat(buffer, "\n");
+        refresh();
+        perror("---------------->Exito al enviar");
+        printf("\n");
+    }
+    pthread_cancel(thread1);
+    return NULL;
+}
+
+void *listen_messages(void *args){
+    lrecv = sizeof(CLIENT);
+    while(memcmp(paqRec, "bye", 3) != 0){
+        tam   = recvfrom(udp_socket, paqRec, PACK_SIZE, 0, (struct sockaddr*)&CLIENT, &lrecv);
+        if(tam == -1){
+            perror("\nError al recibir");
+            exit(0);
+        }
+        printf("|--->>> %s\n", paqRec);
+        strcat(buffer, "--->>> ");
+        strcat(buffer, (char*)paqRec);
+        strcat(buffer, "\n");
+    }
+    pthread_cancel(thread2);
+    return NULL;
+}
+
+int main(){
+    udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(udp_socket == -1){
+        perror("\nError al abrir el socket");
+        return -1;
+    }
+
+    system("clear");
+    perror("\nExito al abrir el socket");
+    SERVER.sin_family = AF_INET;
+    SERVER.sin_port   = htons(SERVER_PORT);
+    SERVER.sin_addr.s_addr = INADDR_ANY;
+    lbind = bind(udp_socket, (struct sockaddr*)&SERVER, sizeof(SERVER));
+
+    if(lbind == -1){
+        perror("\nError en bind");
+        return -1;
+    }
+
+    perror("\nExito en bind");
+    printf("\nEspera a que envien un mensaje\n\n");
+
+    strcpy(buffer, "");
+
+    pthread_create(&thread1, NULL, &listen_messages, "");
+    pthread_create(&thread2, NULL, &send_messages, "");
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    close(udp_socket);
+    return 0;
+}
